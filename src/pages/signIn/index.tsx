37d46a4cc620ec549bcf2b789c4250dc8edc4b0d
konstantin <kostia505@gmail.com>
2024-12-11 18:33:24 +0100

    router: init

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -3,8 +3,14 @@ import FormSignIn from "../../components/formSignIn";
 const SignIn = () => {
   return (
     <>
-      <div>
-        <FormSignIn />
+      <div className="flex justify-between py-[2%] px-[20%]">
+        <div>{/* тут левый блок  */}</div>
+        <div className="w-[50%] flex flex-col justify-center items-center gap-2">
+          <div className="flex flex-col items-center justify-center w-full p-10 border">
+            <FormSignIn />
+          </div>
+          <div className="flex w-full p-10 border items-center justify-center gap-2"></div>
+        </div>
       </div>
     </>
   );
